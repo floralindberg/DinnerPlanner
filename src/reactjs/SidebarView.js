e39930ca304224c3944d133d74c9ea388034aaf3
floralindberg <floral@kth.se>
2024-11-06 19:18:34 +0100

    fromTW1

---
+++ src/reactjs/SidebarView.js
@@ -0,0 +1,23 @@
+import { SidebarView } from "../views/sidebarView";
+import { observer } from "mobx-react-lite";
+
+const Sidebar = observer(             // needed for the presenter to update (its view) when relevant parts of the model change
+    
+    function SidebarRender(props){
+        function changeGuestsACB(number){
+           return props.model.setNumberOfGuests(number);
+        }
+        function setDishACB(dish){
+            return props.model.setCurrentDishId(dish.id);
+            
+        }
+        function removeDishACB(dish){
+            return props.model.removeFromMenu(dish);
+        }
+
+        return <SidebarView number={props.model.numberOfGuests} dishes={props.model.dishes} onNumberChange={changeGuestsACB} intrestedInDish={setDishACB} dishToRemove={removeDishACB}/>;
+       
+    }
+);
+
+export { Sidebar };
---
+++ src/views/sidebarView.jsx
@@ -0,0 +1,70 @@
+
+import "/src/style.css"
+import { dishType } from "../utilities";
+import { menuPrice } from "../utilities";
+import { sortDishes } from "../utilities";
+
+export function SidebarView (props) {
+    
+    function updatePlusACB(){
+        console.log(props.number+1)
+        return props.onNumberChange(props.number+1)
+    }
+
+    function updateMinusACB(){
+        console.log("New number:",props.number-1)
+        return props.onNumberChange(props.number-1)
+    }
+
+    function dishesTableRowCB(dish){
+        function removeDish(evt) {
+            console.log(evt) // incase all of the information about the event is necessary
+            console.log("Dish to remove: ", dish.title)
+            return props.dishToRemove(dish)
+        }
+        function hyperLinkACB(evt){
+            console.log("Clicked dish:",dish.title)
+            return props.intrestedInDish(dish)
+        }
+
+        return ( 
+                    <tr key={dish.id } >
+                        <td> <button onClick={removeDish}>x</button></td>
+                        <td> <a href="#dish.title" onClick={hyperLinkACB}> {dish.title} </a></td>
+                        <td> <span> {dishType(dish)} </span> </td>
+                        <td className="righty">{(dish.pricePerServing*props.number).toFixed(2)} </td>
+                   </tr>
+                )
+               
+    }
+
+    return(
+    <div>
+           <button disabled={props.number == 1} onClick={updateMinusACB}>-</button>
+                <span>
+                   {props.number} 
+                </span>
+            <button  onClick={updatePlusACB}>+</button>  
+
+            <table>
+                <tbody>
+                        {
+                            sortDishes(props.dishes).map(dishesTableRowCB)
+                        }
+                    <tr>
+                        <td></td>
+                        <td> Total:  </td>
+                        <td></td>
+                     <td className="righty">{(menuPrice(props.dishes)*props.number).toFixed(2)}</td>
+                 </tr>
+                </tbody> 
+            </table>
+     
+    </div>
+
+
+)
+    }
+
+
+
