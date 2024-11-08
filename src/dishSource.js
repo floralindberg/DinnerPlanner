ccccf29218df8e1e5170e346c51c11636d478558
elingranath02 <egrana@kth.se>
2024-11-08 13:42:05 +0100

    2.1.2

--- src/dishSource.js
+++ src/dishSource.js
@@ -3,7 +3,8 @@ import { PROXY_KEY } from "./apiConfig";
 
 
 export function searchDishes(searchParams){
-    return fetch(PROXY_URL + "/recipes/complexSearch", {
+    return fetch(PROXY_URL + "/recipes/complexSearch" + "?" + new URLSearchParams(searchParams), {
+        method: "GET",
         headers: {
             "X-DH2642-Key": PROXY_KEY,
             'X-DH2642-Group' : "12",
@@ -13,8 +14,14 @@ export function searchDishes(searchParams){
 
 function gotResponseACB(response){
     console.log(response);
+    return response.json();
 }
 
 function someACB(param){
     console.log(param);
+    return param.results;
+}
+
+export function getMenuDetails(ids_array){
+    
 }
