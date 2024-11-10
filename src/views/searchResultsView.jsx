4d71086fa89e00ea760cdd0e37cb75d471aa1cdb
elingranath02 <egrana@kth.se>
2024-11-10 20:11:24 +0100

    2.2.2

--- src/views/searchResultsView.jsx
+++ src/views/searchResultsView.jsx
@@ -4,32 +4,30 @@ export function SearchResultsView(props){
 
     function renderResultsCB(searchResult){
         return (
-
             <span key={searchResult.id} className="spanet">
 
-                <img src={searchResult.image} height={'100'} className="image"/> 
-                <div>{searchResult.title}</div>
+                <img src={searchResult.image} height={'100'} className="image" onClick={clickedACB}/>
+                <div onClick={clickedACB}>{searchResult.title}</div>
 
             </span>
-
-
         )
-    }
-    return (
 
-        <div className="result">
+        function clickedACB(evt){
+            console.log(searchResult);
+            return searchResult.onDishClicked(searchResult);
+        }
+    }
+
+    return (
+        <div className="result" on>
                 {
-                   props.searchResults.map(renderResultsCB)
+                    props.searchResults.map(renderResultsCB)
                 }
         </div>
     )
 
-}
-
-{/*
-    function renderResultsCB(searchResult){
-        return <div key={searchResult.id}>{searchResult.title + " | " + searchResult.image + " | " + searchResult.id + " | " + searchResult.sourceUrl + " | "}</div>
-            
+    function showsDetailsACB(dishObj){
+        console.log(dishObj.target.value);
     }
-    */
+
 }
