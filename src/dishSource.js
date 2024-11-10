cd5a58a681c0fbf86b5130697f4a0f47b357c37a
elingranath02 <egrana@kth.se>
2024-11-10 10:18:54 +0100

    2.1.3

--- src/dishSource.js
+++ src/dishSource.js
@@ -2,26 +2,51 @@ import { PROXY_URL } from "./apiConfig";
 import { PROXY_KEY } from "./apiConfig";
 
 
-export function searchDishes(searchParams){
+export function searchDishes(searchParams) {
     return fetch(PROXY_URL + "/recipes/complexSearch" + "?" + new URLSearchParams(searchParams), {
-        method: "GET",
         headers: {
             "X-DH2642-Key": PROXY_KEY,
-            'X-DH2642-Group' : "12",
+            'X-DH2642-Group': "12",
         }
     }).then(gotResponseACB).then(someACB);
 }
 
-function gotResponseACB(response){
+function gotResponseACB(response) {
     console.log(response);
     return response.json();
 }
 
-function someACB(param){
+function someACB(param) {
     console.log(param);
     return param.results;
 }
 
-export function getMenuDetails(ids_array){
-    
+export function getMenuDetails(ids_array) {
+    return fetch(PROXY_URL + "/recipes/informationBulk" + "?" + new URLSearchParams({ ids: ids_array.join(",") }), {
+        headers: {
+            "X-DH2642-Key": PROXY_KEY,
+            "X-DH2642-Group": "12",
+        }
+    }).then(menuResponseACB).then(menuACB);
+}
+
+function menuResponseACB(response) {
+    if(!(response.status === 200)) {
+        throw Error("wrong");
+    }
+    console.log(response);
+    return response.json();
+}
+
+function menuACB(param) {
+    console.log(param);
+    return param;
+}
+
+export function getDishDetails(id){
+    return getMenuDetails(id).then(arrToObjACB)
+}
+
+function arrToObjACB(arr){
+
 }
